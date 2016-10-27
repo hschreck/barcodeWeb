a3bbffa6079dd393843bf7865d834231024c98bd
Nationwide Surplus <nationwidesurplus@deploystudio-tower.local>
2016-10-27 14:26:16 -0500

    fixed bug reported. Closes #1

--- printr.php
+++ printr.php
@@ -14,7 +14,7 @@ if ($_POST["sku"] || $_POST["printer"] || $_POST["quantity"]) {
     $referrer = str_replace('?success=false', '', $referrer);
     setcookie('printer', $_POST["printer"]);
     if ($sku != "") {
-        $output = var_dump(shell_exec('barcode -e 128 -b "' . $sku . '" -u in -p "1.45x1" -g 1.75x1 | lpr -P ' . $printer . ' -# ' . $quantity));
+        $output = var_dump(shell_exec('barcode -e 128 -b "' .  escapeshellcmd($sku) . '" -u in -p "1.45x1" -g 1.75x1 | lpr -P ' . escapeshellcmd($printer) . ' -# ' . escapeshellcmd($quantity) ));
         echo ('echo " barcode -e 128 -b "' . $sku . '" -u in -p "2x1" -g 1.7x1" | lpr -P ' . $printer . ' -# ' . $quantity);
         header("location: " . $referrer . "?success=true");
     } else {
